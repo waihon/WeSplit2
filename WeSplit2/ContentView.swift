67987d79e8393c04665f2fe9f98394d5bc9ef7ec
Waihon Yew <yewwaihon@gmail.com>
2022-04-17 18:01:47 +0800

    Add  navigation bar and title

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -9,23 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Line 1")
-                Text("Line 2")
-                Text("Line 3")
-                Text("Line 4")
-                Text("Line 5")
-                Text("Line 6")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Line 1")
+                    Text("Line 2")
+                    Text("Line 3")
+                    Text("Line 4")
+                    Text("Line 5")
+                    Text("Line 6")
+                }
+                Section {
+                    Text("Line 7")
+                    Text("Line 8")
+                    Text("Line 9")
+                    Text("Line 10")
+                    Text("Line 11")
+                    Text("Line 12")
+                }
             }
-            Section {
-                Text("Line 7")
-                Text("Line 8")
-                Text("Line 9")
-                Text("Line 10")
-                Text("Line 11")
-                Text("Line 12")
-            }
+            .navigationTitle("SwiftUI")
         }
     }
 }
