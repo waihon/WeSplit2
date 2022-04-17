2b39f3609c78dab0d0f561be863efe76079e4f26
Waihon Yew <yewwaihon@gmail.com>
2022-04-17 10:55:25 +0800

    Initial Commit

---
+++ WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by Waihon Yew on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
