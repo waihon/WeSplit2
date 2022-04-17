fcaf52b82e65c3f3524005c1489be19b7e553e07
Waihon Yew <yewwaihon@gmail.com>
2022-04-17 17:51:11 +0800

    Use Group to work around the limitation of 10 child views inside a parent

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -10,16 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Form {
-            Text("Line 1")
-            Text("Line 2")
-            Text("Line 3")
-            Text("Line 4")
-            Text("Line 5")
-            Text("Line 6")
-            Text("Line 7")
-            Text("Line 8")
-            Text("Line 9")
-            Text("Line 10")
+            Group {
+                Text("Line 1")
+                Text("Line 2")
+                Text("Line 3")
+                Text("Line 4")
+                Text("Line 5")
+                Text("Line 6")
+            }
+            Group {
+                Text("Line 7")
+                Text("Line 8")
+                Text("Line 9")
+                Text("Line 10")
+                Text("Line 11")
+                Text("Line 12")
+            }
         }
     }
 }
