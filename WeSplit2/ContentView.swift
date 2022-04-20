696cebd7e15a9e7102a5c98b0bfb36e96e52cf52
Waihon Yew <yewwaihon@gmail.com>
2022-04-20 17:12:03 +0800

    Define states and constants with default values

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+
     var body: some View {
         Text("Hello, world!")
             .padding()
