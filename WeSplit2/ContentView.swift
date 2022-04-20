eb168ec0808d78c308563207e7379551e3e03155
Waihon Yew <yewwaihon@gmail.com>
2022-04-20 17:58:19 +0800

    Add a check amount text view to sync with the text field

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -19,6 +19,10 @@ struct ContentView: View {
             Section {
                 TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
             }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
         }
     }
 }
